aa4c024e7f53663db70b2cd8a22ed94d8a1172ec
souta1024 <s.ikeda@cosmowinds.jp>
2020-07-07 15:39:33 +0900

    FormResultとりあえず作成

---
+++ NervousBreakdown/NervousBreakdown/FormResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NervousBreakdown
+{
+    public partial class FormResult : Form
+    {
+        //カウント読み込み?
+        //Count count = new Count();
+
+        //FormMainから受け取る
+        //public string text { get; set; }
+
+        public FormResult()
+        {
+            InitializeComponent();
+        }
+
+        //FormResult起動時
+        //private void FormResult_Load(object sender, EventArgs e)
+        //{
+            //ターン数を受け取る
+            //count.GetCount();
+
+            //TurnLabelに受け取ったターン数を表示
+            //TurnLabel.Text = count.ToString();
+
+            //TurnLabelに受け取ったターン数を表示
+            //TurnLabel.Text = text;
+        //}
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
