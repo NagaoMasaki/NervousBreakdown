93eb52ff7b301719b826e8978d7de1d7525f56c8
mphoikhb <r.iwashita@cosmowinds.jp>
2020-07-09 14:59:00 +0900

    taitle

--- NervousBreakdown/NervousBreakdown/Form1.cs
+++ NervousBreakdown/NervousBreakdown/Form1.cs
@@ -17,12 +17,12 @@ namespace NervousBreakdown
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void box_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
@@ -32,12 +32,7 @@ namespace NervousBreakdown
 
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
