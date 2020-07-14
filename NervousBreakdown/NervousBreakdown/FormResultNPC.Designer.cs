2e01e3ba778c967292c71e6df8c8142014dc210c
miyazaki <t.miyazaki@cosmowinds.jp>
2020-07-14 13:54:23 +0900

    FormResultNPC追加

---
+++ NervousBreakdown/NervousBreakdown/FormResultNPC.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace NervousBreakdown
+{
+    partial class FormResultNPC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shohai = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maisu1 = new System.Windows.Forms.Label();
+            this.maisu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // shohai
+            // 
+            this.shohai.AutoSize = true;
+            this.shohai.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.shohai.ForeColor = System.Drawing.Color.White;
+            this.shohai.Location = new System.Drawing.Point(336, 164);
+            this.shohai.Name = "shohai";
+            this.shohai.Size = new System.Drawing.Size(51, 20);
+            this.shohai.TabIndex = 0;
+            this.shohai.Text = "勝敗";
+            // 
+            // player
+            // 
+            this.player.AutoSize = true;
+            this.player.ForeColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(190, 214);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(36, 12);
+            this.player.TabIndex = 1;
+            this.player.Text = "player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(520, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NPC";
+            // 
+            // maisu1
+            // 
+            this.maisu1.AutoSize = true;
+            this.maisu1.ForeColor = System.Drawing.Color.White;
+            this.maisu1.Location = new System.Drawing.Point(191, 269);
+            this.maisu1.Name = "maisu1";
+            this.maisu1.Size = new System.Drawing.Size(29, 12);
+            this.maisu1.TabIndex = 3;
+            this.maisu1.Text = "枚数";
+            // 
+            // maisu
+            // 
+            this.maisu.AutoSize = true;
+            this.maisu.ForeColor = System.Drawing.Color.White;
+            this.maisu.Location = new System.Drawing.Point(519, 269);
+            this.maisu.Name = "maisu";
+            this.maisu.Size = new System.Drawing.Size(29, 12);
+            this.maisu.TabIndex = 4;
+            this.maisu.Text = "枚数";
+            // 
+            // FormResultNPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maisu);
+            this.Controls.Add(this.maisu1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.shohai);
+            this.Name = "FormResultNPC";
+            this.Text = "FormResultNPC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shohai;
+        private System.Windows.Forms.Label player;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label maisu1;
+        private System.Windows.Forms.Label maisu;
+    }
+}
---
+++ NervousBreakdown/NervousBreakdown/FormResultNPC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NervousBreakdown
+{
+    public partial class FormResultNPC : Form
+    {
+        FormMain formMain;
+
+        //枚数の変数
+        public int playerMaisu = 0;
+        public int npcMaisu = 0;
+
+        public void GetFormMain(FormMain FormMain)
+        {
+            this.formMain = FormMain;
+        }
+
+        //枚数取得
+        public void GetMaisu(int playerCount, int npcCount)
+        {
+            this.playerMaisu = playerCount;
+            this.playerMaisu = npcCount;
+        }
+
+        //FormMainから受け取る
+        public String text { get; set; }
+
+        public FormResultNPC()
+        {
+            InitializeComponent();
+        }
+
+        private void FormResultNPC_Load(object sender, EventArgs e)
+        {
+            maisu1.Text = playerMaisu.ToString();
+            maisu.Text = npcMaisu.ToString();
+            player.Text =text;
+
+            if (playerMaisu > npcMaisu)
+            {
+                shohai.Text = "勝ち";
+            }
+            else if (playerMaisu < npcMaisu)
+            {
+                shohai.Text = "負け";
+            }
+            else
+            {
+                shohai.Text = "引き分け";
+            }
+
+        }
+        private void FormResultNPC_MouseClick(object sender, MouseEventArgs e)
+        {
+            formMain.Close();
+            this.Close();
+            //アプリケーションを終了する
+            Application.Exit();
+        }
+    }
+}
